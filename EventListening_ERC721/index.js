9d6ded9a222f613c6789e69fd2559911ddb4ea96
DESKTOP-4EPP79L\colorverse <rmsdnjs518@colorverse.co>
2023-02-08 10:53:07 +0900

    fix: change address and contract

--- EventListening_ERC721/index.js
+++ EventListening_ERC721/index.js
@@ -8,6 +8,11 @@ let API_URL = "<--alchemy wss URL-->";
 
 const web3 = new Web3(new Web3.providers.IpcProvider('\\\\.\\pipe\\geth.ipc',net));
 
+let Address = '0xB79111ba38eFF0415926ac11bc6f56d087F81E5a';
+//테스트 address
+let Contract = '0x5899a199a50e957430a597d8e7b2136dc1121e7e';
+let TokenId = 1;
+//테스트 Contract
 
 let options721 = {
     topics: [
@@ -36,12 +41,12 @@ subscription721.on('data',event => {
             event.data,
             [event.topics[1], event.topics[2], event.topics[3]]);
 
-            if (transaction.from == '0xcBF099B4EE9145B1c81b6eFe24373F0471CC73da') { console.log('Specified address sent an NFT!') };
+            if (transaction.from == Address) { console.log('Specified address sent an NFT!') };
             //특정 account에서 보낸 transaction
-            if (transaction.to == '0xcBF099B4EE9145B1c81b6eFe24373F0471CC73da') { console.log('Specified address received an NFT!') };
+            if (transaction.to == Address) { console.log('Specified address received an NFT!') };
             //특정 account에서 받은 transaction
-            if (event.address == '0x00000000006c3852cbEf3e08E8dF289169EdE581' && transaction.tokenId == 23090) { console.log('Specified NFT was transferred!') };
-            //특정 event transaction
+            if (event.address == Contract && transaction.tokenId == TokenId) { console.log('Specified NFT was transferred!') };
+            //특정 contract의 특정 tocken의 transaction
 
 
             //블록당 발생하는 전체 ERC-721 출력
